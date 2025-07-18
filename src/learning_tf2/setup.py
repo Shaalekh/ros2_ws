d375b13f0081df378625cfe78badb0e10bb844f1
Aalekh Sharma <shaalekh@gmail.com>
2025-07-18 23:37:29 +0530

    tf2_broadcast

---
+++ src/learning_tf2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'learning_tf2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='shaalekh',
+    maintainer_email='shaalekh@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'static_turtle_tf2_broadcaster=learning_tf2.static_turtle_tf2_broadcaster:main'
+        ],
+    },
+)
